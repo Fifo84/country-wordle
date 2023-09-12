8feca707443cad750bea82615fdcc2db8374cfa4
Ofir Aaronson <fifo84@gmail.com>
2023-09-12 16:27:10 +0300

    added

---
+++ script.js
@@ -0,0 +1,249 @@
+const WORD_LENGTH = 5;
+const FLIP_ANIMATION_DURATION = 500;
+const DANCE_ANIMATION_DURATION = 500;
+const keyboard = document.querySelector("[data-keyboard]");
+const alertContainer = document.querySelector("[data-alert-container]");
+const guessGrid = document.querySelector("[data-guess-grid]");
+let filteredCountryNames = [];
+let randomCountry = "";
+
+async function getRandomCountry() {
+  return fetch("https://restcountries.com/v3/all")
+    .then((res) => res.json())
+    .then((countries) => {
+      const filteredCountries = countries.filter(
+        (country) => country.name.common.length === 5
+      );
+      filteredCountryNames = filteredCountries.map((country) =>
+        country.name.common.toLowerCase()
+      );
+      const randomIndex = Math.floor(
+        Math.random() * filteredCountryNames.length
+      );
+      randomCountry = filteredCountryNames[randomIndex].toLowerCase();
+
+      return { randomCountry, filteredCountries };
+    });
+}
+
+function doSomthing(randomCountry) {
+  console.log(randomCountry);
+}
+
+getRandomCountry();
+
+startInteraction();
+
+function startInteraction() {
+  document.addEventListener("click", handleMouseClick);
+  document.addEventListener("keydown", handleKeyPress);
+}
+
+function stopInteraction() {
+  document.removeEventListener("click", handleMouseClick);
+  document.removeEventListener("keydown", handleKeyPress);
+}
+
+function handleMouseClick(e) {
+  if (e.target.matches("[data-key]")) {
+    pressKey(e.target.dataset.key);
+    return;
+  }
+
+  if (e.target.matches("[data-enter]")) {
+    submitGuess();
+    return;
+  }
+
+  if (e.target.matches("[data-delete]")) {
+    deleteKey();
+    return;
+  }
+}
+
+function handleKeyPress(e) {
+  if (e.key === "Enter") {
+    submitGuess();
+    return;
+  }
+
+  if (e.key === "Backspace" || e.key === "Delete") {
+    deleteKey();
+    return;
+  }
+
+  if (e.key.match(/^[a-z]$/)) {
+    pressKey(e.key);
+    return;
+  }
+}
+
+function pressKey(key) {
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length >= WORD_LENGTH) return;
+  const nextTile = guessGrid.querySelector(":not([data-letter])");
+  nextTile.dataset.letter = key.toLowerCase();
+  nextTile.textContent = key;
+  nextTile.dataset.state = "active";
+}
+
+function deleteKey() {
+  const activeTiles = getActiveTiles();
+  const lastTile = activeTiles[activeTiles.length - 1];
+  if (lastTile == null) return;
+  lastTile.textContent = "";
+  delete lastTile.dataset.state;
+  delete lastTile.dataset.letter;
+}
+
+function submitGuess() {
+  const activeTiles = [...getActiveTiles()];
+  if (activeTiles.length !== WORD_LENGTH) {
+    showAlert("Not Enough Letters");
+    shakeTiles(activeTiles);
+    return;
+  }
+  const guess = activeTiles.reduce((word, tile) => {
+    return word + tile.dataset.letter;
+  }, "");
+
+  const lowercaseGuess = guess.toLowerCase();
+
+  // console.log(lowercaseGuess);
+  // console.log(randomCountry);
+  // console.log(filteredCountryNames);
+
+  if (!filteredCountryNames.includes(lowercaseGuess)) {
+    showAlert("Not in country list");
+    shakeTiles(activeTiles);
+    return;
+  }
+
+  // if (filteredCountryNames.includes(lowercaseGuess)) {
+  //   showAlert("This country is in the list"); // Display alert for correct guess
+  // }
+
+  stopInteraction();
+  activeTiles.forEach((...params) => flipTile(...params, lowercaseGuess));
+}
+
+function flipTile(tile, index, array, guess) {
+  const letter = tile.dataset.letter;
+  const key = keyboard.querySelector(`[data-key="${letter}"i]`);
+  setTimeout(() => {
+    tile.classList.add("flip");
+  }, (index * FLIP_ANIMATION_DURATION) / 2);
+
+  tile.addEventListener(
+    "transitionend",
+    () => {
+      tile.classList.remove("flip");
+      if (randomCountry[index] === letter) {
+        tile.dataset.state = "correct";
+        key.classList.add("correct");
+      } else if (randomCountry.includes(letter)) {
+        tile.dataset.state = "wrong-location";
+        key.classList.add("wrong-location");
+      } else {
+        tile.dataset.state = "wrong";
+        key.classList.add("wrong");
+      }
+
+      if (index === array.length - 1) {
+        tile.addEventListener(
+          "transitionend",
+          () => {
+            startInteraction();
+            checkWinLose(guess, array);
+          },
+          { once: true }
+        );
+      }
+    },
+    { once: true }
+  );
+}
+
+function getActiveTiles() {
+  return guessGrid.querySelectorAll('[data-state="active"]');
+}
+
+function showAlert(message, duration = 1000) {
+  const alert = document.createElement("div");
+  alert.textContent = message;
+  alert.classList.add("alert");
+  alertContainer.prepend(alert);
+  if (duration == null) return;
+
+  setTimeout(() => {
+    alert.classList.add("hide");
+    alert.addEventListener("transitionend", () => {
+      alert.remove();
+    });
+  }, duration);
+}
+
+function shakeTiles(tiles) {
+  tiles.forEach((tile) => {
+    tile.classList.add("shake");
+    tile.addEventListener(
+      "animationend",
+      () => {
+        tile.classList.remove("shake");
+      },
+      { once: true }
+    );
+  });
+}
+
+function checkWinLose(lowercaseGuess, tiles) {
+  if (lowercaseGuess === randomCountry) {
+    showAlert("You Win", 5000);
+    danceTiles(tiles);
+    stopInteraction();
+    return;
+  }
+
+  const remainingTiles = guessGrid.querySelectorAll(":not([data-letter])");
+  if (remainingTiles.length === 0) {
+    showAlert(
+      "You lose. the correct country was:" + randomCountry.toLocaleUpperCase(),
+      null
+    );
+    stopInteraction();
+  }
+}
+
+function danceTiles(tiles) {
+  tiles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.classList.add("dance");
+      tile.addEventListener(
+        "animationend",
+        () => {
+          tile.classList.remove("dance");
+        },
+        { once: true }
+      );
+    }, (index * DANCE_ANIMATION_DURATION) / 5);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const howToPlayButton = document.getElementById("howToPlayButton");
+  const howToPlayModal = document.getElementById("howToPlayModal");
+  const howToPlayModalClose = document.querySelector(
+    "#howToPlayModal .modal-close"
+  );
+  howToPlayButton.addEventListener("click", () => {
+    howToPlayModal.style.display = "flex";
+  });
+
+  // Show the "How to Play" modal on page load
+  howToPlayModal.style.display = "flex";
+
+  // Close the "How to Play" modal when clicking the close button
+  howToPlayModalClose.addEventListener("click", () => {
+    howToPlayModal.style.display = "none";
+  });
+});
